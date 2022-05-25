b3812fcc9ce9a7872055866296a1d8758d54e0d6
RiotOff <85680090+RiotOff@users.noreply.github.com>
2022-05-25 13:31:39 +0300

    Update UILibrary.lua

--- UILibrary.lua
+++ UILibrary.lua
@@ -1,4 +1,4 @@
--- Hexagon UI Library
+-- OnzxV UI Library
 local TweenService, RunService, UserInputService,gui,dragging,dragInput,dragStart,startPos,cpt,cpf,cppicking,cppickingVal,cppickingAlpha,cphue,cpsat,cpval,focused,highest,focusedBox = game:GetService("TweenService"),game:GetService("RunService"), game:GetService("UserInputService")
 local cpalpha = 0
 
@@ -155,10 +155,10 @@ local library = {
 	windows = {},
     pointers = {},
     settings = {
-        guiname = "OnxzV",
-		title = "OnzxV [Special Edition]",
-		logo = "rbxassetid://4350178803", -- "http://www.roblox.com/asset/?id=6597976562",
-        footer = " RiotOff Production $$$",
+        guiname = "OnxzV©",
+		title = "OnzxV [Premium Edition]",
+		logo = "8941979235, -- "https://www.roblox.com/library/8941979235/Riots-Community",
+        footer = " By RiotOff",
         modal = true,
         font = Enum.Font.SourceSans,
         textsize = 16,
